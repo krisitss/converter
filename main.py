9df1bbb8e5b508a0f5c28daac17977f14b79c55a
krisits <kreslinskrisjanis@gmail.com>
2025-09-03 11:17:54 +0300

    v1

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+THIS IS CONVERTER
+"""
+# converts F value to C value
+def convertCtoF():
+    C = float(input("ievadi C:"))
+    F = (C*9/5) +32
+    print(F)
---
+++ readme.txt
@@ -0,0 +1,1 @@
+Krisjanis Kreslins. Veidoju konvertoru.
